cbad24e1762a982d1ca180eee01a17d88f73f72e
vicente.garcia <vbgarciag@gmail.com>
2016-11-11 09:28:08 -0600

    add htaccess and views

---
+++ CodeIgniter/application/views/login.php
@@ -0,0 +1,7 @@
+<section class="well section_login">
+	<article class="article-md">	
+		<input type="email" class="form-control" name="" id="" placeholder="Email"><br>
+		<input type="password" class="form-control" name="" id="" placeholder="Password"><br>
+		<burtton class="btn btn-primary">SIGN IN</burtton>
+	</article>		
+</section>
